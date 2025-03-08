028abd5e552c79f0fe15dd887f24408f6d4b3d9a
Kathis64 <116247198+Kathis64@users.noreply.github.com>
2025-03-08 15:04:30 -0500

    primer commit

---
+++ punto1taller.py
@@ -0,0 +1,7 @@
+celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+
+Fahrenheit = (celsius * 9/5) + 32
+Kelvin = celsius + 273.15
+
+print(f"Fahrenheit: {Fahrenheit}")
+print(f"Kelvin: {Kelvin}")
